693417f43fce11a9f80fd268f3350ae8c704b64a
FernandoM <m87.fernando@gmail.com>
2024-05-12 20:48:18 -0400

    feat: update main , using manager

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"warp-forge/manager"
 	"warp-forge/task"
 	"warp-forge/worker"
 
@@ -30,7 +31,60 @@ func main() {
 	go runTasks(&w)
 	go w.CollectStats()
 	api.Start()
+
+	workers := []string{fmt.Sprintf("%s:%d", host, port)}
+	m := manager.New(workers)
+
+	for i := 0; i < 3; i++ {
+		t := task.Task{
+			ID:    uuid.New(),
+			Name:  fmt.Sprintf("test-container-%d", i),
+			State: task.Scheduled,
+			Image: "fbscarel/myapp-color",
+		}
+		te := task.TaskEvent{
+			ID:    uuid.New(),
+			State: task.Running,
+			Task:  t,
+		}
+		m.AddTask(te)
+		m.SendWork()
+	}
+
+	go func() {
+		for {
+			fmt.Printf("[Manager] Updating tasks from %d workers\n", len(m.Workers))
+			m.UpdateTasks()
+			time.Sleep(15 * time.Second)
+		}
+	}()
+
+	for {
+		for _, t := range m.TaskDb {
+			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
+			time.Sleep(15 * time.Second)
+		}
+	}
 }
+
+/*
+	func main() {
+		host := os.Getenv("CUBE_HOST")
+		port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
+
+		fmt.Println("Starting Cube worker")
+
+		w := worker.Worker{
+			Queue: *queue.New(),
+			Db:    make(map[uuid.UUID]*task.Task),
+		}
+		api := worker.Api{Address: host, Port: port, Worker: &w}
+
+		go runTasks(&w)
+		go w.CollectStats()
+		api.Start()
+	}
+*/
 func runTasks(w *worker.Worker) {
 	for {
 		if w.Queue.Len() != 0 {
